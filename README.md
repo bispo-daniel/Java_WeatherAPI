f05ded22eb96baefc53c1d94d97690a6a1850d5b
bispo-daniel <dbispo.work@gmail.com>
2023-04-27 14:12:10 -0300

    update
    README commit
    MySecretKey & pom.mxl reorganization

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Java_WeatherAPI
- 
+    Java application to query a city weather.
+
+    The API used is from Weather API.
+    //Get your key here:
+
+    https://www.weatherapi.com/my/
+
+# About
+    This project was developed as a need of mine to learn to developed and handle HTTP requisitions with Java.
+    This project used Maven to use some dependencies.
+    Swing was used to build a user-friendly GUI.
+
+    3 dependencies has been used:
+        com.squareup.okhttp3 -- To get the API's response
+        org.json -- To transform the API's Response into a JSON
+        com.jayway.jsonpath -- To get values from the JSON
+    
+    The greatest challenge here was the JSON handling, then jsonpath was implemented and things got easier.
+
+    A separate class was created (MySecretKey.java) to hide my API key that is used in Main.java
+
+# Screenshots
+
+## `Main menu`
+![all-text](https://github.com/bispo-daniel/Java_WeatherAPI/blob/main/src/Screenshots/MenuScreenshot.png)
+
+## `Query menu`
+![all-text](https://github.com/bispo-daniel/Java_WeatherAPI/blob/main/src/Screenshots/QueryPageScreenshot.png)
--- src/projectPack/Main.java
+++ src/projectPack/Main.java
@@ -3,6 +3,7 @@ package projectPack;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import org.json.JSONObject;
@@ -43,9 +44,11 @@ public class Main {
 			JsonPath jsonPathLocationCountry = JsonPath.compile("$.location.country");
 			Object objectLocationCountry= jsonPathLocationCountry.read(obj, config);
 			
-			JsonPath jsonPathCurrentConditionIcon = JsonPath.compile("$.current.condition.icon");
-			Object objectCurrentConditionIcon = jsonPathCurrentConditionIcon.read(obj, config);
+			JsonPath jsonPathLocationLocalTime = JsonPath.compile("$.location.localtime");
+			Object objectLocationLocalTime = jsonPathLocationLocalTime.read(obj, config);
 			
+			/**/
+						
 			JsonPath jsonPathCurrentConditionText = JsonPath.compile("$.current.condition.text");
 			Object objectCurrentConditionText = jsonPathCurrentConditionText.read(obj, config);
 			
@@ -59,11 +62,13 @@ public class Main {
 			jsonValues.add(objectLocationRegion.toString());
 			jsonValues.add(objectLocationCountry.toString());
 			
-			jsonValues.add(objectCurrentConditionIcon.toString());
+			jsonValues.add(objectLocationLocalTime.toString());
+			
 			jsonValues.add(objectCurrentConditionText.toString());
 			jsonValues.add(objectCurrentTemp.toString());
 			jsonValues.add(objectCurrentWind.toString());
 		
+
 		} else {
 			JOptionPane.showMessageDialog(null, "Something different happened...\nTry again!");
 			main(null);
@@ -74,22 +79,24 @@ public class Main {
 
 	 static void menu() throws IOException {
 		String menuDialog = "Hello, This is the Java Weather App!!\n\n Type a city name to display it's weather \n Type 0 to exit";
-	 	String query = JOptionPane.showInputDialog(null, menuDialog);
-
+	 	String query = JOptionPane.showInputDialog(null, menuDialog, "Weather App", JOptionPane.QUESTION_MESSAGE);
+	 			
 		if("0".equals(query)){
 			System.exit(0);
 		} else {
-			MySecretURL urlWithKey = new MySecretURL();
-			String url = urlWithKey.URL;
-			String formatedURL = String.format(url, query);
+			MySecretKey mySecretKey = new MySecretKey();
+			String secretKey = mySecretKey.KEY;
+			
+			String url = "https://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no";
+			String formatedURL = String.format(url, secretKey, query);
 
 			ArrayList<String> response = getJSON(formatedURL);
 
-			String queryDialog = " %s - %s - %s \n %s - %s°C - Wind: %s KM/H";
-			String formatedDialog = String.format(queryDialog, response.get(0), response.get(1), response.get(2), response.get(4), response.get(5), response.get(6));
+			String queryDialog = "<html><body><div align='center'> %s - %s - %s <br> %s - %s°C - Wind: %s KM/H <br> Local time: %s </div></body></html>";
+			String formatedDialog = String.format(queryDialog, response.get(0), response.get(1), response.get(2), response.get(4), response.get(5), response.get(6), response.get(3));
 			JOptionPane.showMessageDialog(null, formatedDialog,
 			        "Weather App",
-			        JOptionPane.INFORMATION_MESSAGE);
+			        JOptionPane.INFORMATION_MESSAGE, new ImageIcon());
 
 		}
 
